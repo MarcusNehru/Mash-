582e3470087be061cb8b06c0101daa3261268200
MarcusNehru <marcusaustin2001@gmail.com>
2018-07-13 12:55:46 -0700

    final project week 1

---
+++ mash.js
@@ -0,0 +1,26 @@
+function mash(){
+    return "house"    
+}
+ console.log(mash())
+
+ let array = ["High Class Mansion","house","Shack","Apartment",process.argv[2]];
+ function gethome(){
+    return array[random()]
+ }
+function random(){
+ let decimal = Math.random()
+const random = decimal *array.length;
+const final = Math.floor(random);
+return final
+}
+console.log(gethome())
+
+
+function getChildrenCount(){
+    let number = math.random()
+    const times100 = number* 100
+    return times100;
+
+
+}
+console.log (getChildrenCount)
